f1f814531965b2c523b64e96ef18411bec885794
Natalie Jane Pacificar <nataliejane.pacificar@outlook.com>
2023-04-13 14:41:38 +0800

    added all files

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import cardData from '../src/data/data.json'
+import SimpleCard from './components/SimpleCard'
+
+export default function App() {
+
+
+  return (
+    <>
+      <div className="flex flex-col w-full bg-slate-500 min-h-screen">
+
+        {/* Container 1 */}
+        <div className='max-w-7xl w-full mx-auto min-h-screen px-7 py-8'>
+          <div className='flex gap-x-8 items-start flex-wrap gap-y-8'>
+            {
+              cardData.map(item => (
+                <SimpleCard {...item}></SimpleCard>
+              ))
+            }
+          </div>
+        </div>
+
+
+        {/* Container 2 */}
+        {/* <div className='bg-blue-400 h-screen'>
+          hi
+        </div> */}
+
+        {/* Container 3 */}
+        {/* <div className='bg-yellow-400 h-screen'>
+          hi
+        </div> */}
+
+      </div>
+    </>
+  )
+}
---
+++ src/components/SimpleCard.tsx
@@ -0,0 +1,32 @@
+interface ISimpleCard {
+    imgUrl: string,
+    title: string,
+    description: string,
+    tags: string[]
+}
+
+const SimpleCard: React.FC<ISimpleCard> = ({ imgUrl, title, description, tags }) => {
+    return (
+        <>
+            <div className="w-[350px] rounded-3xl bg-white overflow-hidden">
+                <div className="w-full h-64 relative">
+                    <img src={imgUrl} className="absolute object-cover w-full h-full" />
+                </div>
+                <div className="p-6 pb-8">
+                    <div className="text-lg mb-2 font-medium">{title}</div>
+                    <div className="text-sm">{description}</div>
+
+                    <div className="pt-4 flex flex-wrap justify-items-start gap-2">
+                        {tags.map((tag, i) => (
+                            <div key={i} className=" rounded border border-indigo-400 bg-indigo-100 px-2.5 py-0.5 text-center text-xs font-medium text-indigo-800 dark:bg-gray-700 dark:text-indigo-400"> {tag} </div>
+                        ))}
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default SimpleCard;
